bbbfc5c8bcfbf426f97f9e94c96d3dbb397806f7
JerinXplore <jerinjerin101325@gmail.com>
2025-04-18 16:04:12 +0600

    stored wishlist to local storage and showed toast alert

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoredDB } from '../../utility/addToDB';
 
 const BookDetails = () => {
     const {id} = useParams();
@@ -7,13 +8,23 @@ const BookDetails = () => {
     const data = useLoaderData();
     // console.log(data);
     const singleBook = data.find(book=>book.bookId===bookId);
-    const {bookName,image}=singleBook;
+    const {bookName,image}=singleBook || {};
+
+    const handleMarkAsRead = id=>{
+        // store with id
+        // where to store
+        // array or collection
+        // if book already exist then show a alert
+        // if book not exist then push in the collection or array
+        addToStoredDB(id);
+    }
+
     return (
         <div className='w-2/3 mx-auto'>
             <img className='w-48' src={image} alt="" />
             <h5>{bookName}</h5>
-            <button className="btn btn-soft m-2 btn-info">Read</button>
-            <button className="btn btn-soft m-2 btn-success">Wishlist</button>
+            <button onClick={()=>handleMarkAsRead(id)} className="btn btn-soft m-2 btn-info">Mark as Read</button>
+            <button className="btn btn-soft m-2 btn-success">Add to Wishlist</button>
         </div>
     );
 };
