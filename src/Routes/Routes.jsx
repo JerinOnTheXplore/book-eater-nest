fd99c3a1326edd7f9124a0f8fb9166e785214212
JerinXplore <jerinjerin101325@gmail.com>
2025-04-18 16:41:34 +0600

    added react tabs

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,7 @@ import ErrorPage from '../Pages/Root/ErrorPage/ErrorPage';
 import Home from '../Pages/Root/Home/Home';
 import About from '../Pages/Root/About/About';
 import BookDetails from '../Pages/BookDetails/BookDetails';
+import ReadList from '../Pages/ReadList/ReadList';
 export const router=createBrowserRouter([
     {
       path:"/",
@@ -22,6 +23,10 @@ export const router=createBrowserRouter([
         {
           path:'/about',
           Component:About,
+        },
+        {
+          path:'readList',
+          Component:ReadList,
         },
         {
           path:'/bookDetails/:id',
