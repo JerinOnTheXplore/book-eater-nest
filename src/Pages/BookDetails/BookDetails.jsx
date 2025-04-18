0e1ec226188645982b16650b12d56b34e3025c63
JerinXplore <jerinjerin101325@gmail.com>
2025-04-18 14:26:47 +0600

    used params hook and fetched data dynamically

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+    const {id} = useParams();
+    const bookId=parseInt(id);
+    const data = useLoaderData();
+    // console.log(data);
+    const singleBook = data.find(book=>book.bookId===bookId);
+    const {bookName,image}=singleBook;
     return (
-        <div>
-            <h1>Bookdetails</h1>
+        <div className='w-2/3 mx-auto'>
+            <img className='w-48' src={image} alt="" />
+            <h5>{bookName}</h5>
+            <button className="btn btn-soft m-2 btn-info">Read</button>
+            <button className="btn btn-soft m-2 btn-success">Wishlist</button>
         </div>
     );
 };
